8675b7b2789696b32b81712a1f44af6731a5eae2
iShubham-Sams <shubhambhandari12345hb@gmail.com>
2023-05-02 07:15:21 +0530

    computing power complete

---
+++ Mathamatics/computingpower.js
@@ -0,0 +1,21 @@
+const computingPoewer = (num, pow) => {
+  let result = 1;
+  for (let i = 1; i <= pow; i++) {
+    result = result * num;
+  }
+  return result;
+};
+// console.log(computingPoewer(5, 0));
+
+const efficentAns = (num, pow) => {
+  if (pow == 0) 1;
+  let halfPow = Math.floor(pow / 2);
+  let temp = Math.pow(num, halfPow);
+  temp = temp * temp;
+  if (pow % 2 == 0) {
+    return temp;
+  } else {
+    return temp * num;
+  }
+};
+console.log(efficentAns(5, 8));
